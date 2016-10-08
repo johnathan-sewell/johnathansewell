ca7b2ed6b722069cd41aa0440a67dccdedaf4442
jonathansewell <jonathanssewell@gmail.com>
2016-10-08 23:19:24 +0200

    restructure for templating + new posts

---
+++ bin/template.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+const fs = require('fs');
+const shell = require('shelljs');
+
+const partialsArgs = fs.readdirSync('./templates/partials/')
+  .filter(f => f.endsWith('.template'))
+  .reduce((args, partial) => `${args} -p templates/partials/${partial}`, '');
+
+fs.readdirSync('./templates/')
+  .filter(filePath => filePath.endsWith('.template'))
+  .forEach((templatePath) => {
+    const templateName = templatePath.substring(0, templatePath.lastIndexOf('.template'));
+    const outputDir = templateName === 'index' ? 'public/' : 'public/writing/';
+    const command = `$(npm bin)/mustache ${partialsArgs} templates/data/empty.json templates/${templatePath} > ${outputDir}/${templateName}.html`;
+    console.log(command); // eslint-disable-line no-console
+    shell.exec(command);
+  });
